37575dae141bf3c444f33ea82b988f880048c236
Soroush Kami <soroushkami10@gmail.com>
2023-10-17 06:12:25 -0400

    add player turns

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import { useState } from "react";
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner is " + winner;
+  }
+  else {
+    status = "Next player is " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i){
+
+    if (squares[i] || calculateWinner[squares]) {
+      return;
+    }
+
     const copySquares = squares.slice(); // make a copy of squares array
-    copySquares[i] = 'X';
+    if (xIsNext) {
+      copySquares[i] = 'X';
+    } 
+    else {
+      copySquares [i] = 'O';
+    }
+
     setSquares(copySquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
   <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -34,9 +57,31 @@ export default function Board() {
   );
 }
 
+
 function Square({value, onSquareClick}){
 
   return (
     <button className="square" onClick={onSquareClick}>{value}</button>
   );
+}
+
+
+function calculateWinner(squares){
+  const wLines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i=0; i < wLines.length; i++){
+    const [a,b,c] = wLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
